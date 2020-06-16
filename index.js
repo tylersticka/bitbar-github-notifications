5acf1fedd60ffb59e726aabe5cd85cf47d343167
Tyler Sticka <tyler@tylersticka.com>
2020-06-15 17:26:37 -0700

    Restore "mark as read" option

--- index.js
+++ index.js
@@ -1,33 +1,28 @@
 const minimist = require("minimist");
 const bitbar = require("bitbar");
 const { getItems } = require("./lib/items");
-// const { markAsRead } = require("./lib/mark-as-read");
+const { markAsRead } = require("./lib/mark-as-read");
+
 
 const defaults = {
   icon: "octoface",
   apiRoot: "https://api.github.com/",
   group: true,
   inboxHref: "https://github.com/notifications",
+  scriptBase: process.argv.slice(0, 2).join(" "),
 };
 
-// const scriptBase = process.argv.slice(0, 2).join(" ");
 const processArgs = minimist(process.argv.slice(2));
 
 async function plugin(options) {
   const settings = { ...defaults, ...options, ...processArgs };
+
+  if (settings.action === 'read') {
+    return markAsRead(settings);
+  }
+
   const items = await getItems(settings);
   bitbar(items);
-
-  // if (count) {
-  //   items.push({
-  //     text: "Mark all as read",
-  //     templateImage: icons["primitive-dot-stroke"],
-  //     bash: scriptBase,
-  //     param1: "--action=read",
-  //     refresh: true,
-  //     terminal: false,
-  //   });
-  // }
 }
 
 module.exports = plugin;
